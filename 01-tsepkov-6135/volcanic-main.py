39d76dba40442d616bfeb1b88548dbad33acbf55
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-09-14 19:24:50 +0400

    udpate

--- 01-tsepkov-6135/volcanic-main.py
+++ 01-tsepkov-6135/volcanic-main.py
@@ -1,16 +1,24 @@
 from altair.vegalite.v4.schema.channels import Column
+from numpy import angle
 import pandas as pd
 from pydeck.bindings import map_styles
 from pydeck.bindings.map_styles import DARK, LIGHT, ROAD, SATELLITE
 import streamlit as st
-import plotly.express as px
+import altair as alt
 from urllib.request import urlopen
 import json
 import pydeck as pdk
+from sympy import source
+
+st.set_page_config(
+    page_title="Vulcans visualization",
+    layout="wide"
+)
 
 def preprocessing():
     df = pd.read_csv("Volcanic Eruptions in the Holocene Period.csv",\
         usecols=["Name","Country","Region","Type","Activity Evidence","Last Known Eruption","Latitude","Longitude","Elevation (Meters)"])
+    df = df.dropna()
     return df
 
 def main():
@@ -18,25 +26,28 @@ def main():
     with st.expander("Click for view dataset"):
         st.dataframe(df)
     with st.container():
-        cols = st.columns([2,1])
+        source = df.groupby(["Country"]).agg({"Name":"count"}).sort_values("Name",ascending=False).iloc[:10].reset_index()
+        chart = alt.Chart(source,title="Top 10 country by count vulcans").mark_bar().encode(
+            x=alt.X("Country:N", sort="-y"),y=alt.Y("Name:Q")
+        )
+        text = chart.mark_text(
+            align="center",
+            baseline="middle",
+            dy=15,
+        ).encode(
+            text="Name:Q",
+            color=alt.ColorValue("white")
+        )
+        cols = st.columns([3,1])
         with cols[0]:
-            filt_activity = st.sidebar.selectbox("Choose volcans by Activity type", df["Activity Evidence"].unique())
-            chart_data = df.query("`Activity Evidence` == @filt_activity")
-            chart = px.histogram(data_frame=chart_data,x="Country",title="Count of Volcanic by {}".format(filt_activity))
-            st.plotly_chart(chart,True)
-            chart_data = df[["Longitude","Latitude","Activity Evidence"]].query("`Activity Evidence` == @filt_activity").dropna()
-            lays = pdk.Layer(
-                "TextLayer",\
-                chart_data,\
-                get_text="Name",\
-                get_size=16,\
-                get_position=["Longitude","Latitude"])
-            st.pydeck_chart(pdk.Deck(layers=[lays],tooltip={f"text":"{Name}"}))
+            st.altair_chart(chart+text, True)
         with cols[1]:
-            filt_country = st.sidebar.selectbox("Choose volcans by Country type", df["Country"].unique())
-            chart_data = df.query("`Country`==@filt_country")
-            chart = px.pie(data_frame=chart_data,names="Activity Evidence",title="Distribution of Volcanic type by {}".format(filt_country))
-            st.plotly_chart(chart,True)
-        
+            st.altair_chart(
+                alt.Chart(source).mark_bar().encode(
+                    x=alt.X("Name:N"), y=alt.Y("Elevation (Meters):Q")
+                ),True
+            )
+
+
 if __name__=='__main__':
     main()
